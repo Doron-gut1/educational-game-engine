11b85fbf02232af88f1efb56ddf0d85bf7d17ad8
Doron-gut1 <hodayyae@gmail.com>
2025-03-20 21:36:26 +0200

    הוספת קובץ אינדקס לשירותים
    יצירת קובץ מרכזי לייצוא כל השירותים במערכת:
    - מאפשר ייבוא מרוכז של שירותים
    - מייעל את הקוד ומפחית כפילויות
    - מגדיר נקודת כניסה אחידה לכל השירותים

---
+++ src/services/index.js
@@ -0,0 +1,15 @@
+/**
+ * שירותים מרכזיים - ייצוא מרוכז
+ */
+
+// ניהול לוגים
+export * from './loggerService';
+
+// טעינת תוכן
+export * from './contentLoader';
+
+// שירותי משוב
+export * from './feedbackService';
+
+// שירותי אחסון
+export * from './storageService';
