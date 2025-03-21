3077934ecf5f66596871896b2b47243f71c0ada9
Doron-gut1 <hodayyae@gmail.com>
2025-03-21 08:55:09 +0200

    הוספת קובץ אינדקס מרכזי לרכיבי מערכת העיצוב

---
+++ src/design-system/components/index.js
@@ -0,0 +1,12 @@
+/**
+ * ייצוא מרכזי של כל רכיבי מערכת העיצוב
+ */
+
+// רכיבי כפתורים
+export * from './buttons';
+
+// רכיבי כרטיסים
+export * from './cards';
+
+// רכיבי ניווט
+export * from './navigation';
