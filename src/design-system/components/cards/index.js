0a1f4035faf9cc13754d4cc61cdf1a09f0f24afe
Doron-gut1 <hodayyae@gmail.com>
2025-03-23 09:41:09 +0200

    Update cards index to export Card component

--- src/design-system/components/cards/index.js
+++ src/design-system/components/cards/index.js
@@ -1,5 +1,6 @@
 /**
  * ייצוא מרוכז של כל רכיבי הכרטיסים במערכת
  */
+export { default as Card } from './Card';
 export { default as ScrollCard } from './ScrollCard';
 export { default as GlassCard } from './GlassCard';
