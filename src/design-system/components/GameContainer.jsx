e31b94f10c8e12748f59134c67a62d9ecc01e669
Doron-gut1 <hodayyae@gmail.com>
2025-03-23 11:14:34 +0200

    הוספת רכיב GameContainer החסר

--- src/design-system/components/GameContainer.jsx
+++ src/design-system/components/GameContainer.jsx
@@ -1,33 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card } from './Card';
 
 /**
- * רכיב מיכל (container) לתוכן המשחק
+ * רכיב מיכל לאזור משחק
  */
 export const GameContainer = ({
   children,
+  variant = 'default',
+  padding = 'medium',
+  shadow = 'medium',
   className = '',
+  style = {},
   ...props
 }) => {
+  // מיפוי גדלי ריפוד
+  const paddingSizes = {
+    none: 'p-0',
+    small: 'p-2',
+    medium: 'p-4',
+    large: 'p-6',
+    xlarge: 'p-8'
+  };
+  
+  // משתמש בבסיס של Card עם הוספת אפיונים שונים
   return (
-    <div
-      className={`container mx-auto p-4 rounded-lg shadow-lg ${className}`}
+    <Card
+      variant={variant}
+      shadow={shadow}
+      className={`rounded-xl ${paddingSizes[padding]} ${className}`}
+      style={style}
       {...props}
     >
       {children}
-    </div>
+    </Card>
   );
 };
 
 GameContainer.propTypes = {
   /**
-   * תוכן המשחק
+   * תוכן אזור המשחק
    */
   children: PropTypes.node,
   /**
+   * סגנון הכרטיסייה
+   */
+  variant: PropTypes.oneOf(['default', 'primary', 'secondary', 'accent', 'translucent', 'passover', 'tubishvat']),
+  /**
+   * גודל הריפוד הפנימי
+   */
+  padding: PropTypes.oneOf(['none', 'small', 'medium', 'large', 'xlarge']),
+  /**
+   * עוצמת הצל
+   */
+  shadow: PropTypes.oneOf(['none', 'small', 'medium', 'large']),
+  /**
    * className נוסף
    */
-  className: PropTypes.string
+  className: PropTypes.string,
+  /**
+   * סגנון מותאם אישית
+   */
+  style: PropTypes.object
 };
 
 export default GameContainer;
